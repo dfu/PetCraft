3ff472cd14d7b7f333f6afc6121fc797e777ff68
dfu <dbruce@gmail.com>
2010-11-06 05:02:27 -0500

    Updated listener

--- PetListener.java
+++ PetListener.java
@@ -6,13 +6,34 @@ import java.lang.reflect.*;
 import java.util.Random;
 
 public class PetListener extends PluginListener {
-	private static final Hashtable <Player, 
+	private static final Hashtable <Player, Pet> petsByPlayer = new Hashtable<Player, Pet>();
+
 
 	public boolean onCommand(Player player, String[] split) {
-		if(split[0].equalsIgnoreCase("/petcreate"))
-			if(player.canUseCommand(split[0])) {
-
+		if(!player.canUseCommand(split[0]))
+			return false;
+		if(split[0].equalsIgnoreCase("/petcreate")) {
+			if(split[1] == null) {
+				player.sendMessage("Usage: /petcreate petname");
+				return false;
 			}
+			Pet pet = petsByPlayer.get(player);
+			if(pet != null) {
+				player.sendMessage("You already have a pet!");
+				return false;
+			}
+			pet = new Pet(player, split[1]);
+			petsByPlayer.put(player, pet);
+			pet.start();
+			return true;
+		}
+		return false;
+	}
+
+	public void onPlayerMove(Player player, Location from, Location to) {
+		Pet pet = petsByPlayer.get(player);
+		if (pet != null)
+			pet.onPlayerMove(from, to);
 
 	}
 }
